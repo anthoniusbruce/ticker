be392d2f78002be47dec203803661a9339fa88a9
anthoniusbruce <anthonius.bruce@gmail.com>
2024-10-23 16:25:09 -0400

    re-arranged process_symbols in preparation for multiple threads

--- src/symbol_processor.rs
+++ src/symbol_processor.rs
@@ -9,20 +9,30 @@ pub mod symbol_processor {
 
     /// using the list of symbols get the daily quotes for the past month
     pub fn process_symbols(symbols: Vec<&str>, output_dir: &PathBuf) {
+        let one_day = time::Duration::days(1);
+        let one_month = time::Duration::days(30);
+        let today = OffsetDateTime::now_utc();
+        let end_date = today - one_day;
+        let start_date = end_date - one_month;
         for symbol in symbols {
-            let one_day = time::Duration::days(1);
-            let one_month = time::Duration::days(30);
-            let today = OffsetDateTime::now_utc();
-            let end_date = today - one_day;
-            let start_date = end_date - one_month;
-            let quotes = get_quotes(symbol, &start_date, &end_date);
-            let mut gains = Vec::new();
-            for quote in quotes {
-                let gain = get_gain(quote);
-                gains.push(gain);
-            }
-            save_gains(output_dir, symbol, gains);
+            process_one_symbol(symbol, output_dir, start_date, end_date);
         }
+    }
+
+    /// Function that will process 1 symbol from http call to yahoo to save the file to local storage
+    fn process_one_symbol(
+        symbol: &str,
+        output_dir: &PathBuf,
+        start_date: OffsetDateTime,
+        end_date: OffsetDateTime,
+    ) {
+        let quotes = get_quotes(symbol, start_date, end_date);
+        let mut gains = Vec::new();
+        for quote in quotes {
+            let gain = get_gain(quote);
+            gains.push(gain);
+        }
+        save_gains(output_dir, symbol, gains);
     }
 
     /// saves the daily gains to a csv file in the output directory
@@ -57,7 +67,7 @@ pub mod symbol_processor {
     }
 
     /// Method to get that quotes over a duration for a given ticker symbol
-    fn get_quotes(symbol: &str, start: &OffsetDateTime, end: &OffsetDateTime) -> Vec<Quote> {
+    fn get_quotes(symbol: &str, start: OffsetDateTime, end: OffsetDateTime) -> Vec<Quote> {
         let quotes = Vec::new();
         let provider_result = yahoo_finance_api::YahooConnector::new();
         match provider_result {
@@ -68,7 +78,7 @@ pub mod symbol_processor {
                     Err(e) => log(symbol, e),
                     Ok(builder) => {
                         let resp_result =
-                            builder.block_on(provider.get_quote_history(symbol, *start, *end));
+                            builder.block_on(provider.get_quote_history(symbol, start, end));
                         match resp_result {
                             Err(e) => log(symbol, e),
                             Ok(resp) => match resp.quotes() {
